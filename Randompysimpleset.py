e22eb78e0f461df19a55c41b4048420cbfe1b65c
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2026-03-27 13:13:15 +0530

    Update Randompysimpleset.py

--- Randompysimpleset.py
+++ Randompysimpleset.py
@@ -10,6 +10,8 @@ while 1:
 
 #Next we have to update a matrix based on conditionals on input a 
 
+#Simplest maze solver if we solve in Python for humans it can be modified for phone or anything with artificial intelligence code transform 
+
 Floodfillmatrix=np.zeros(100,100)
 
 if(a=='rightarrow'):
