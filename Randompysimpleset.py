d5763fb271d0d716866fa74c7e22de4672d265f7
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2026-03-27 12:34:44 +0530

    Update Randompysimpleset.py

--- Randompysimpleset.py
+++ Randompysimpleset.py
@@ -1,5 +1,16 @@
 import random
 
+import numpy as np
+
 while 1:
   print(random.choice(['left','right','straight','back'])
   a=input()
+
+
+
+\\Next we have to update a matrix based on conditionals on a 
+
+Floodfillmatrix=np.zeros(100,100)
+
+if(a=='rightarrow'):
+  print('I don't know how to code flood fill')
